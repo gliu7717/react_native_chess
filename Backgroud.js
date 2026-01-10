9c5e968bdbbc573c5e4e9895cc6ba3941b9714cb
rayliu7717 <rayliu2004@gmail.com>
2026-01-10 11:52:05 -0500

    update version

--- Backgroud.js
+++ Backgroud.js
@@ -1,27 +1,27 @@
 import React from "react";
-import { View, StyleSheet,Dimensions,Image} from "react-native";
+import { View, StyleSheet, Dimensions, Image } from "react-native";
 
-const { width,height } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 export const SIZE = width;
-const TOPMARGIN = (height - width) /2 -  width / 9 + 10
+const TOPMARGIN = (height - width) / 2 - width / 9 - 10;
 
 const Background = () => {
-    return (
-      <View style={{ flex: 1,backgroundColor:"transparent" }}>
-        <Image source = {require("./assets/board.png")} style = {styles.board} />
-      </View>
-    );
-  };
+  return (
+    <View style={{ flex: 1, backgroundColor: "transparent" }}>
+      <Image source={require("./assets/board.png")} style={styles.board} />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
   },
   board: {
-    marginTop:TOPMARGIN,
+    marginTop: TOPMARGIN,
     width: SIZE,
     height: SIZE,
-    backgroundColor:"transparent"
+    backgroundColor: "transparent",
   },
 });
 export default Background;
